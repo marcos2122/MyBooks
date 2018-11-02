3d0fc1d2dab28a34d089adaef3edf696fec9758a
mjime <mjimeno2122@gmail.com>
2018-11-02 16:42:29 +0100

    Crear Notificaciones para ANDROID 8.0 superior

--- app/src/main/java/mjimeno/mybooks2/Red/MyFirebaseMessagingService.java
+++ app/src/main/java/mjimeno/mybooks2/Red/MyFirebaseMessagingService.java
@@ -1,5 +1,7 @@
 package mjimeno.mybooks2.Red;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -7,13 +9,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import mjimeno.mybooks2.Activities.BookDetailActivity;
 import mjimeno.mybooks2.Activities.BookListActivity;
+import mjimeno.mybooks2.Activities.LoginActivity;
+import mjimeno.mybooks2.Activities.PresentationActivity;
+import mjimeno.mybooks2.Activities.SplashActivity;
 import mjimeno.mybooks2.R;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
@@ -26,41 +33,59 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         String from = remoteMessage.getFrom();
         Log.d(TAG,"Mensaje recibido de" + from);
 
-        if (remoteMessage.getNotification()!= null)
-        {
-            Log.d(TAG,"Notificacion: " + remoteMessage.getNotification().getBody());
+             if (remoteMessage.getNotification()!= null) {
+                 Log.d(TAG, "Notificacion: " + remoteMessage.getNotification().getBody());
 
-            sendNotification(remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody());
-        }
-        if (remoteMessage.getData().size()>0)
-        {
-            Log.d(TAG,"Data: " +remoteMessage.getData());
-        }
+                 sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+              }
 
+            if (remoteMessage.getData().size()>0)
+              {
+              Log.d(TAG,"Data: " +remoteMessage.getData());
 
-
+              }
 
     }
 
     private void sendNotification(String title, String body) {
-        Intent intent = new Intent(this,BookListActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
+        Log.d(TAG,"Cuerponotif: " +body);
+
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id));
+
+        Intent ii = new Intent(this, PresentationActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, ii, 0);
         Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,
-                getString(R.string.default_notification_channel_id))
-                .setSmallIcon(R.mipmap.ic_launcher3)
-                .setContentTitle(title)
-                .setContentText(body)
-                .setAutoCancel(true)
-                .setSound(soundUri)
-                .setContentIntent(pendingIntent);
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0,notificationBuilder.build());
+
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setSmallIcon(R.mipmap.ic_launcher_round);
+        mBuilder.setAutoCancel(true);
+        mBuilder.setContentTitle(title);
+        mBuilder.setContentText(body);
+        mBuilder.setSound(soundUri);
+        mBuilder.setPriority(Notification.PRIORITY_MAX);
 
 
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            //para android oreo se necesita NotificationChannely NotificationCompat.Builder(this)está en desuso
+
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+
+            NotificationChannel channel = new NotificationChannel(getString(R.string.default_notification_channel_id),name,importance);
+            channel.setDescription(description);
+
+            mNotificationManager.createNotificationChannel(channel);
+        }
+
+        mNotificationManager.notify(0, mBuilder.build());
 
 
 
